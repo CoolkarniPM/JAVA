4ca85f6f1453433b313ff918c2e7225f6adeaae8
lenovo <lenovo@LAPTOP-Q9ROR1R0>
2023-04-07 10:24:23 +0530

    OKK

---
+++ javaprogramming/src/com/stringassignment/Replacevowel.java
@@ -0,0 +1,25 @@
+package com.stringassignment;
+
+
+	public class Replacevowel {
+		public static void replace(String s) {
+			String s2 = s.toLowerCase();
+			char ch[] = s.toCharArray();
+
+			for (int i = 0; i < ch.length; i++) {
+				if (ch[i] == 'a' || ch[i] == 'e' || ch[i] == 'i' || ch[i] == 'o' || ch[i] == 'u') {
+					ch[i] = '@';
+				}
+			}
+			System.out.println(ch);
+		}
+
+		public static void main(String[] args) {
+			String s = "java";
+			replace(s);
+
+		}
+
+	}
+
+
