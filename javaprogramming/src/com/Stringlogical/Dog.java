c57aa6518f5adce9afca947e23315c25a75291ab
CoolkarniPM <124145731+CoolkarniPM@users.noreply.github.com>
2023-04-19 09:07:32 +0530

    Add files via upload

---
+++ javaprogramming/src/com/Stringlogical/Dog.java
@@ -0,0 +1,14 @@
+package inhheritance;
+
+public class Dog extends Animal {
+	void bark() {
+		System.out.println("barking");
+	}
+
+	public void eat() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
+}
---
+++ javaprogramming/src/com/Stringlogical/Testinheritance.java
@@ -0,0 +1,13 @@
+package inhheritance;
+
+public class Testinheritance {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Dog d=new Dog();
+		d.bark();
+		d.eat();
+
+	}
+
+}
